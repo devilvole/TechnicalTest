a5ed41dc34f9aea162cda59374ef73d7b54c2d40
Jonathan Humphries <Jonathan.Humphries@storm-technologies.com>
2023-04-02 19:34:47 +0100

    still messing with test sqlite

--- ServiceTests/CustomerServiceTests.cs
+++ ServiceTests/CustomerServiceTests.cs
@@ -1,3 +1,5 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using TechnicalTest.API;
 using TechnicalTest.API.Services;
 using TechnicalTest.Data;
@@ -6,7 +8,18 @@ namespace ServiceTests
 {
 	public class CustomerServiceTests
 	{
-		ICustomerService service = new CustomerService(new ApplicationContext());
+		ICustomerService service;
+
+		public CustomerServiceTests()
+		{
+
+			var options = new DbContextOptionsBuilder<ApplicationContext>()
+				.UseSqlite("Data Source=database.db;")
+				.Options;
+			ApplicationContext context = new(options);
+			service = new CustomerService(context);
+			context.Database.EnsureCreated();
+		}
 
 		[Fact]
 		public async void TestAdd()
--- TechnicalTest.Data/ApplicationContext.cs
+++ TechnicalTest.Data/ApplicationContext.cs
@@ -4,6 +4,10 @@ namespace TechnicalTest.Data;
 
 public class ApplicationContext : DbContext
 {
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) 
+    { 
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data Source=bin\\database.db;");
